69cfd4229b9471be92c93198f3e638ba5a1520cf
Luis Meza <luismeza01@gmail.com>
2016-11-09 20:45:20 -0600

    adding beginning information

---
+++ js/scripts.js
@@ -0,0 +1,80 @@
+  // Map API //
+  var map;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {
+        lat: 30.257927,
+        lng: -97.742520
+      },
+      zoom: 14,
+      mapTypeControl: false,
+      disableDoubleClickZoom: true,
+      scrollwheel: false,
+      disableDefaultUI: true,
+      draggable: false,
+      styles: [{
+        elementType: 'geometry',
+        stylers: [{
+          color: '#242f3e'
+        }]
+      }, {
+        elementType: 'labels.text.stroke',
+        stylers: [{
+          color: '#242f3e'
+        }]
+      }, {
+        elementType: 'labels.text.fill',
+        stylers: [{
+          color: '#746855'
+        }]
+      }, {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [{
+          color: '#38414e'
+        }]
+      }, {
+        featureType: "road",
+        elementType: "labels",
+        stylers: [{
+          visibility: "off"
+        }]
+      }, {
+        featureType: "poi",
+        elementType: "labels",
+        stylers: [{
+          visibility: "off"
+        }]
+      }, {
+        featureType: "transit",
+        elementType: "all",
+        stylers: [{
+          visibility: "off"
+        }]
+      }, {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [{
+          color: '#746855'
+        }]
+      }, {
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [{
+          color: '#1f2835'
+        }]
+      }, {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [{
+          color: '#17263c'
+        }]
+      }, {
+        featureType: 'water',
+        elementType: 'labels.text.stroke',
+        stylers: [{
+          color: '#17263c'
+        }]
+      }]
+    });
+  }
